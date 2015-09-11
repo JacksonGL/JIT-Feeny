8a58fd2877ca85763608480d647c3e464481fab4
Ben Mehne <bmehne@gmail.com>
2015-09-11 00:59:44 -0700

    Refactored for increased type safety and added assert for error printing

--- src/interpreter.c
+++ src/interpreter.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-#include <assert.h>
 #include "utils.h"
 #include "ast.h"
 #include "interpreter.h"
@@ -11,19 +10,61 @@
 // implementation of interpreter functions
 //----------------------------------------
 
+void slot_assert(int i, SlotStmt* s, const char * fmt, ...){
+	va_list args;
+	va_start(args, fmt);
+	if(!i){
+		printf("Encountered error: ");
+		vprintf(fmt, args);
+		printf(" in slot statement ");
+		print_slotstmt(s);
+		printf("\n");
+		exit(-1);
+	}
+	va_end(args);
+}
+
+void scope_assert(int i, ScopeStmt* s, const char * fmt, ...){
+	va_list args;
+	va_start(args, fmt);
+	if(!i){
+		printf("Encountered error: ");
+		vprintf(fmt, args);
+		printf(" in scope statement ");
+		print_scopestmt(s);
+		printf("\n");
+		exit(-1);
+	}
+	va_end(args);
+}
+
+void exp_assert(int i, Exp* s, const char * fmt, ...){
+	va_list args;
+	va_start(args, fmt);
+	if(!i){
+		printf("Encountered error: ");
+		vprintf(fmt, args);
+		printf(" in expression ");
+		print_exp(s);
+		printf("\n");
+		exit(-1);
+	}
+	va_end(args);
+}
+
 void interpret (ScopeStmt* s) {
   EnvObj* genv = get_global_env_obj();
-  Obj* result = eval_stmt(genv, genv, s);
+  eval_stmt(genv, genv, s);
 }
 
 Obj* eval_stmt (EnvObj* genv, EnvObj* env, ScopeStmt* s) {
   switch (s->tag) {
   case VAR_STMT: {
-    exec_var_stmt(genv, env, s);
+    exec_var_stmt(genv, env, (ScopeVar*)s);
     return (Obj*)make_null_obj();
   }
   case FN_STMT: {
-    exec_fn_stmt(genv, env, s);
+    exec_fn_stmt(genv, env, (ScopeFn*)s);
     return (Obj*)make_null_obj();
   }
   case SEQ_STMT: {
@@ -36,7 +77,7 @@ Obj* eval_stmt (EnvObj* genv, EnvObj* env, ScopeStmt* s) {
     return eval_exp(genv, env, s2->exp);
   }
   default:
-    printf("Unrecognized scope statement with tag %d\n", s->tag);
+    scope_assert(0, s, "Unrecognized scope statement with tag %d", s->tag);
     exit(-1);
   }
 }
@@ -44,92 +85,81 @@ Obj* eval_stmt (EnvObj* genv, EnvObj* env, ScopeStmt* s) {
 Obj* eval_exp (EnvObj* genv, EnvObj* env, Exp* e) {
   switch (e->tag) {
   case INT_EXP: {
-    return eval_int_exp(genv, env, e);
+    return eval_int_exp(genv, env, (IntExp*)e);
   }
   case NULL_EXP: {
-    return eval_null_exp(genv, env, e);
+    return (Obj*) make_null_obj();
   }
   case PRINTF_EXP: {
-    return eval_printf(genv, env, e);
+    return eval_printf(genv, env, (PrintfExp*)e);
   }
   case ARRAY_EXP: {
-    return eval_array_exp(genv, env, e);
+    return eval_array_exp(genv, env, (ArrayExp*)e);
   }
   case OBJECT_EXP: {
-    return eval_obj_exp(genv, env, e);
+    return eval_obj_exp(genv, env, (ObjectExp*)e);
   }
   case SLOT_EXP: {
-    return eval_slot_exp(genv, env, e);
+    return eval_slot_exp(genv, env, (SlotExp*)e);
   }
   case SET_SLOT_EXP: {
-    return eval_set_slot_exp(genv, env, e);
+    return eval_set_slot_exp(genv, env, (SetSlotExp*)e);
   }
   case CALL_SLOT_EXP: {
-    return eval_call_slot_exp(genv, env, e);
+    return eval_call_slot_exp(genv, env, (CallSlotExp*)e);
   }
   case CALL_EXP: {
     return eval_call_exp(genv, env, (CallExp*)e);
   }
   case SET_EXP: {
-    return eval_set_exp(genv, env, e);
+    return eval_set_exp(genv, env, (SetExp*)e);
   }
   case IF_EXP: {
-    return eval_if_exp(genv, env, e);
+    return eval_if_exp(genv, env, (IfExp*)e);
   }
   case WHILE_EXP: {
-    return eval_while_exp(genv, env, e);
+    return eval_while_exp(genv, env, (WhileExp*)e);
   }
   case REF_EXP: {
-    return eval_ref_exp(genv, env, e);
+    return eval_ref_exp(genv, env, (RefExp*)e);
   }
   default:
-    printf("Unrecognized Expression with tag %d\n", e->tag);
-    exit(-1);
+    exp_assert(0, e, "Unrecognized Expression with tag %d", e->tag);
   }
   return NULL;
 }
 
-Obj* eval_set_slot_exp (EnvObj* genv, EnvObj* env, Exp* e) {
-  SetSlotExp* e2 = (SetSlotExp*)e;
+Obj* eval_set_slot_exp (EnvObj* genv, EnvObj* env, SetSlotExp* e2) {
   Obj* obj_ptr = eval_exp(genv, env, e2->exp);
-  if (obj_type(obj_ptr) != ENV_OBJ) {
-    printf("Cannot get slot %s from non-env object.", e2->name);
-    exit(-1);
-  }
+  exp_assert(obj_type(obj_ptr)==ENV_OBJ, (Exp*)e2,
+    "Cannot get slot %s from non-env object", e2->name);
+
   Entry* entry = get_entry((EnvObj*)obj_ptr, e2->name);
-  if (entry == NULL || entry_type(entry) != VAR_ENTRY) {
-    printf("Var slot %s does not exist in object.", e2->name);
-    exit(-1);
-  }
+
+  exp_assert(entry!=NULL && entry_type(entry) == VAR_ENTRY, (Exp*) e2,
+    "Var slot %s does not exist in object", e2->name);
+
   Obj* val = eval_exp(genv, env, e2->value);
   set_value((VarEntry*)entry, val);
   return val;
 }
 
-Obj* eval_slot_exp (EnvObj* genv, EnvObj* env, Exp* e) {
-  SlotExp* e2 = (SlotExp*)e;
+Obj* eval_slot_exp (EnvObj* genv, EnvObj* env, SlotExp* e2) {
   Obj* obj_ptr = eval_exp(genv, env, e2->exp);
-  if (obj_type(obj_ptr) != ENV_OBJ) {
-    printf("Cannot get slot %s from non-env object.", e2->name);
-    exit(-1);
-  }
+
+  exp_assert(obj_type(obj_ptr) == ENV_OBJ, (Exp*) e2, "Cannot get slot %s from non-env object", e2->name);
+
   Entry* entry = get_entry((EnvObj*)obj_ptr, e2->name);
-  if (entry == NULL || entry_type(entry) != VAR_ENTRY) {
-    printf("Var slot %s does not exist in object.", e2->name);
-    exit(-1);
-  }
+  exp_assert(entry!= NULL && entry_type(entry)==VAR_ENTRY,
+    (Exp*) e2, "Var slot %s does not exist in object", e2->name);
   return get_value((VarEntry*)entry);
 }
 
-Obj* eval_set_exp (EnvObj* genv, EnvObj* env, Exp *e) {
-  SetExp* e2 = (SetExp*)e;
+Obj* eval_set_exp (EnvObj* genv, EnvObj* env, SetExp *e2) {
   VarEntry* var_entry = (VarEntry*)get_entry(env, e2->name);
   if (var_entry == NULL) {
     var_entry = (VarEntry*)get_entry(genv, e2->name);
-    if (var_entry == NULL) {
-      printf("Entry %s is not defined.", e2->name);
-      exit(-1);
-    }
+    exp_assert(var_entry!= NULL, (Exp*) e2, "Entry %s is not defined", e2->name);
   }
 
   Obj* new_val_ptr = eval_exp(genv, env, e2->exp);
@@ -137,20 +167,19 @@ Obj* eval_set_exp (EnvObj* genv, EnvObj* env, Exp *e) {
   return new_val_ptr;
 }
 
-Obj* eval_while_exp (EnvObj* genv, EnvObj* env, Exp *e) {
-  WhileExp* e2 = (WhileExp*)e;
+Obj* eval_while_exp (EnvObj* genv, EnvObj* env, WhileExp *e2) {
   Obj* cond_ptr = NULL;
   Obj* result = (Obj*)make_null_obj();
   while (1) {
     cond_ptr = eval_exp(genv, env, e2->pred);
-    if (cond_ptr == NULL) {
-      printf("Invalid conditional value");
-      exit(-1);
-    }
-    if (obj_type(cond_ptr) != NULL_OBJ) {
+
+	 exp_assert(cond_ptr != NULL, (Exp*) e2, "Invalid conditional value");
+
+	 if (obj_type(cond_ptr) != NULL_OBJ) {
       // create branch environment/scope
       EnvObj* body_env = make_env_obj((Obj*)env);
       result = eval_stmt(genv, body_env, e2->body);
+		free_env_obj(body_env);
     } else {
       break;
     }
@@ -158,52 +187,48 @@ Obj* eval_while_exp (EnvObj* genv, EnvObj* env, Exp *e) {
   return result;
 }
 
-Obj* eval_if_exp (EnvObj* genv, EnvObj* env, Exp *e) {
-  IfExp* e2 = (IfExp*)e;
+Obj* eval_if_exp (EnvObj* genv, EnvObj* env, IfExp *e2) {
   Obj* result = NULL;
   // create branch environment/scope
   EnvObj* branch_env = make_env_obj((Obj*)env);
 
   Obj* cond_ptr = eval_exp(genv, env, e2->pred);
-  if (cond_ptr == NULL) {
-    printf("Invalid conditional value");
-    exit(-1);
-  }
+  exp_assert(cond_ptr != NULL, (Exp*) e2, "Invalid conditional value");
+
   if (obj_type(cond_ptr) != NULL_OBJ) {
     result = eval_stmt(genv, branch_env, e2->conseq);
   } else {
     result = eval_stmt(genv, branch_env, e2->alt);
   }
   // TODO: free the branch environment object
+  free_env_obj(branch_env);
   return result;
 }
 
 Obj* eval_call_exp (EnvObj* genv, EnvObj* env, CallExp*e) {
   Entry* t = get_entry(genv, e->name);
-  assert(entry_type(t) == CODE_ENTRY);
+  exp_assert(entry_type(t) == CODE_ENTRY, (Exp*) e, "Can only call methods/functions!");
   ScopeFn* f = get_scope_fn((CodeEntry*)t);
   // feeny is a statically scoped language
   EnvObj * fnEnv = make_env_obj((Obj*)genv);
-  assert(fnEnv != env);
-  assert(e->nargs == f->nargs);
+  exp_assert(e->nargs == f->nargs, (Exp*) e,
+    "Wrong number of arguments!");
   for (int i = 0; i < e->nargs; ++i) {
     Obj* r = eval_exp(genv, env, e->args[i]);
     add_entry(fnEnv, f->args[i], (Entry*)make_var_entry(r));
-    assert(get_entry(fnEnv, f->args[i]) != NULL);
+    exp_assert(get_entry(fnEnv, f->args[i]) != NULL, (Exp*) e,
+      "Failed to evaluated %ith argument!", i);
   }
   Obj* result = eval_stmt(genv, fnEnv, f->body);
   //TODO: free fnEnv
+  free_env_obj(fnEnv);
   return result;
 }
 
-Obj* eval_call_slot_exp (EnvObj* genv, EnvObj* env, Exp *e) {
-  Obj* first_arg_obj_ptr = NULL;
-  Obj* second_arg_obj_ptr = NULL;
-
-  CallSlotExp* e2 = (CallSlotExp*)e;
+Obj* eval_call_slot_exp (EnvObj* genv, EnvObj* env, CallSlotExp *e2) {
   Obj *receiver_ptr = eval_exp(genv, env, e2->exp);
-  char *method_name_ptr = e2->name;
-  assert(obj_type(receiver_ptr) != NULL_OBJ);
+  exp_assert(obj_type(receiver_ptr) != NULL_OBJ, (Exp*) e2,
+    "Null object does not have methods!");
   inc_method_call(receiver_ptr);
 
   // TODO: make the error message more informative
@@ -211,14 +236,10 @@ Obj* eval_call_slot_exp (EnvObj* genv, EnvObj* env, Exp *e) {
   // handle built in functions
   case INT_OBJ: {
     Obj* arg;
-    if (e2->nargs != 1 ||
-        obj_type(arg = eval_exp(genv, env, e2->args[0])) != INT_OBJ) {
-      printf("native int function error - %s\n",
+	 exp_assert(e2->nargs == 1 && obj_type(arg = eval_exp(genv, env, e2->args[0])) == INT_OBJ,
+      (Exp*) e2, "native int function error - %s",
              e2->nargs != 1 ? "not enough arguments!" :
-             "wrong argument type!"
-            );
-      exit(-1);
-    }
+             "wrong argument type!");
     if (!strcmp(e2->name, "add")) {
       return (Obj*) int_obj_add((IntObj*) receiver_ptr, (IntObj*)arg);
     } else if (!strcmp(e2->name, "sub")) {
@@ -240,8 +261,7 @@ Obj* eval_call_slot_exp (EnvObj* genv, EnvObj* env, Exp *e) {
     } else if (!strcmp(e2->name, "eq")) {
       return (Obj*) eq((IntObj*) receiver_ptr, (IntObj*)arg);
     } else {
-      printf("unknown native int function\n");
-      exit(-1);
+      exp_assert(0, (Exp*) e2, "unknown native int function");
     }
   }
   case ARRAY_OBJ: {
@@ -249,67 +269,58 @@ Obj* eval_call_slot_exp (EnvObj* genv, EnvObj* env, Exp *e) {
       return (Obj*)array_length((ArrayObj*) receiver_ptr);
     }
     Obj* first_arg;
-    if (e2->nargs < 1 || obj_type(first_arg = eval_exp(genv, env, e2->args[0])) != INT_OBJ) {
-      printf("native array function error - %s\n",
+	 exp_assert(e2->nargs > 0 && obj_type(first_arg = eval_exp(genv, env, e2->args[0])) == INT_OBJ,
+      (Exp*) e2, "native array function error - %s",
              e2->nargs < 1 ? "not enough arguments!" :
              "incorrect argument type!");
-      exit(-1);
-    }
     if (!strcmp(e2->name, "set") && e2->nargs == 2) {
       return (Obj*)array_set((ArrayObj*)receiver_ptr,
                              (IntObj*)first_arg, eval_exp(genv, env, e2->args[1]));
     } else if (!strcmp(e2->name, "get") && e2->nargs == 1) {
       return (Obj*)array_get((ArrayObj*)receiver_ptr, (IntObj*)first_arg);
     } else {
-      printf("unknown native array function\n");
-      exit(-1);
+      exp_assert(0, (Exp*) e2, "unknown native array function");
     }
   }
   case ENV_OBJ: {
     Entry* code = get_entry((EnvObj*)receiver_ptr, e2->name);
-    assert(code && entry_type(code) == CODE_ENTRY);
+	 exp_assert(code && entry_type(code) == CODE_ENTRY, (Exp*)e2,
+      "Failed to get code for method!");
     ScopeFn* f = get_scope_fn((CodeEntry*)code);
     EnvObj * fnEnv = make_env_obj((Obj*)genv);
-    assert(fnEnv != env);
     for (int i = 0; i < e2->nargs; ++i) {
       Obj* r = eval_exp(genv, env, e2->args[i]);
       add_entry(fnEnv, f->args[i], (Entry*)make_var_entry(r));
-      assert(get_entry(fnEnv, f->args[i]) != NULL);
+      exp_assert(get_entry(fnEnv, f->args[i]) != NULL, (Exp*) e2,
+        "Failed to evaluated %ith argument!", i);
     }
     add_entry(fnEnv, "this", (Entry*) make_var_entry(receiver_ptr));
     Obj* result = eval_stmt(genv, fnEnv, f->body);
     //TODO: free fnEnv
+	 free_env_obj(fnEnv);
     return result;
   }
   default:
     // TODO: complain about null obj
-    assert(0);
+    exp_assert(0, (Exp*) e2, "Cannot call method on null object!");
+    exit(-1);
   }
 }
 
-Obj* eval_null_exp (EnvObj* genv, EnvObj* env, Exp *e) {
-  return (Obj*)make_null_obj();
-}
-
-Obj* eval_int_exp (EnvObj* genv, EnvObj* env, Exp *e) {
-  IntExp* e2 = (IntExp*)e;
+Obj* eval_int_exp (EnvObj* genv, EnvObj* env, IntExp *e2) {
   return (Obj*)make_int_obj(e2->value);
 }
 
-Obj* eval_array_exp (EnvObj* genv, EnvObj* env, Exp* e) {
-  ArrayExp* e2 = (ArrayExp*)e;
+Obj* eval_array_exp (EnvObj* genv, EnvObj* env, ArrayExp* e2) {
   Obj* array_length_ptr = eval_exp(genv, env, e2->length);
-  if (obj_type(array_length_ptr) != INT_OBJ) {
-    printf("call array() function: array length is not an integer.\n");
-    exit(-1);
-  }
+  exp_assert(obj_type(array_length_ptr) == INT_OBJ, (Exp*) e2,
+    "call array() function: array length is not an integer");
   Obj* init_ptr = eval_exp(genv, env, e2->init);
   return (Obj*) make_array_obj((IntObj*)array_length_ptr, init_ptr);
 }
 
-Obj* eval_printf (EnvObj* genv, EnvObj* env, Exp* e) {
+Obj* eval_printf (EnvObj* genv, EnvObj* env, PrintfExp* e2) {
   char replace_char[] = "~";
-  PrintfExp* e2 = (PrintfExp*)e;
   // replace all ~ in format string into the corresponding arguments
   char *new_str = copy_string(e2->format);
   for (int i = 0; i < e2->nexps; i++) {
@@ -328,25 +339,18 @@ Obj* eval_printf (EnvObj* genv, EnvObj* env, Exp* e) {
   return (Obj*)make_null_obj();
 }
 
-void exec_stmt (EnvObj* genv, EnvObj* env, ScopeStmt* s) {
-  printf("Why us be called?\n");
-}
-
-void exec_fn_stmt (EnvObj* genv, EnvObj* env, ScopeStmt* s) {
-  ScopeFn* s2 = (ScopeFn*)s;
+void exec_fn_stmt (EnvObj* genv, EnvObj* env, ScopeFn* s2) {
   CodeEntry* code_entry = make_code_entry(s2);
   add_entry(env, s2->name, (Entry*)code_entry);
 }
 
-void exec_var_stmt (EnvObj* genv, EnvObj* env, ScopeStmt* s) {
-  ScopeVar* s2 = (ScopeVar*)s;
+void exec_var_stmt (EnvObj* genv, EnvObj* env, ScopeVar* s2) {
   Obj* obj_ptr = eval_exp(genv, env, s2->exp);
   VarEntry* var_entry = make_var_entry(obj_ptr);
   add_entry(env, s2->name, (Entry*)var_entry);
 }
 
-Obj* eval_obj_exp (EnvObj* genv, EnvObj* env, Exp* e) {
-  ObjectExp* e2 = (ObjectExp*)e;
+Obj* eval_obj_exp (EnvObj* genv, EnvObj* env, ObjectExp* e2) {
   EnvObj* nenv = make_env_obj(eval_exp(genv, env, e2->parent));
   for (int i = 0; i < e2->nslots; i++) {
     switch (e2->slots[i]->tag) {
@@ -360,22 +364,19 @@ Obj* eval_obj_exp (EnvObj* genv, EnvObj* env, Exp* e) {
   return (Obj*)nenv;
 }
 
-Obj* eval_ref_exp (EnvObj* genv, EnvObj* env, Exp* e) {
-  RefExp* e2 = (RefExp*)e;
+Obj* eval_ref_exp (EnvObj* genv, EnvObj* env, RefExp* e2) {
   // try to locate the entry in the local scope
   Entry* entry_ptr = get_entry(env, e2->name);
   if (entry_ptr == NULL) {
     // try to locate the entry in the global scope
     entry_ptr = get_entry(genv, e2->name);
-    if (entry_ptr == NULL) {
-      printf("undefined reference: %s", e2->name);
-      exit(-1);
-    }
+    exp_assert(entry_ptr != NULL, (Exp*) e2,
+      "undefined reference: %s", e2->name);
   }
   if (entry_type(entry_ptr) == VAR_ENTRY) {
     return get_value((VarEntry*)entry_ptr);
   } else {
-    printf("cannot refer to code entry");
+    exp_assert(0, (Exp*) e2, "cannot refer to code entry");
     exit(-1);
   }
 }
