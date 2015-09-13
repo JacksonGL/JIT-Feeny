93d9d48ef190fd0df24da33a7256f14d883e6cda
JacksonGL <jacksongl1988@gmail.com>
2015-09-13 16:16:32 -0700

    delete original files in the root directory

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gcc src/cfeeny.c src/utils.c src/ast.c src/interpreter.c -o cfeeny -Wno-int-to-void-pointer-cast
-
-./parser_osx -i tests/unit_test/test1.feeny -oast output/cplx.ast
-
-./cfeeny output/cplx.ast
