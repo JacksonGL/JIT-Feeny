256e84514098621ff50b42cc73e9b9d23ab0aa05
Liang Gong <gongliang13@corvette.CS.Berkeley.EDU>
2015-11-11 19:30:20 -0800

    assembly code for goto and branch

--- JITEx/src/vm.s
+++ JITEx/src/vm.s
@@ -29,3 +29,66 @@ c_trap:
 	movq $0xcafebabecafebabe, %rax
 	ret
 after_c_trap:
+
+.globl exec_goto_op_1
+.globl exec_goto_op_end_1
+
+exec_goto_op_1:
+	movl    4(%rdi), %eax
+##      movq    $0xcafebabecafebabe, %rax
+        ret
+exec_goto_op_end_1:
+
+.globl exec_branch_op_1
+.globl exec_branch_op_end_1
+
+## branch operation
+exec_branch_op_1:
+## stack pop
+	movq    stack_pointer(%rip), %rdx
+        leaq    -8(%rdx), %r10
+        movq    %r10, stack_pointer(%rip)
+        movq    -8(%rdx), %rax
+## stack pop end
+## obj_type start
+        cmpq    $2, %rax
+        jne     V_NEQ_2
+## return NULL_OBJ
+## return pc+1;
+        movq    %rsi, %rax ## replace %rsi with a hole for pc
+        incq	%rax
+        ret
+V_NEQ_2:
+## v%2
+        andq    $1, %rax
+        jz      RET_NOT_NULL
+## if (v%2 == 1)
+## get tag start
+## IValue* tv = (((uintptr_t)v) & CLEAR_ARRAY_OBJ_MASK);
+        andq    $0xFFFFFFFFFFFFFFF8, %rax
+## return tv->_tag;
+        movq    (%rax), %rdx
+        movq    %rdx, %rax
+## get tag end
+## compare OBJ_OBJ and tag
+        cmpq    $4, %rax
+        jbe     RET
+## if (tag > OBJ_OBJ)
+## return pc+1;
+        movq    %rsi, %rax ## replace $rsi with a hole for pc
+        incq    %rax
+        ret
+RET:
+        cmpq    $1, %rax
+        jne     RET_NOT_NULL
+## obj_type end
+RET_NULL:
+## return pc+1;
+        movq    %rsi, %rax ## replace %rsi with a hole for pc
+        incq    %rax
+        ret
+RET_NOT_NULL:
+## return i->name;
+        movl    4(%rdi), %eax  ## replace 4(%rdi) for a hole for i->name
+        ret
+exec_branch_op_end_1:
