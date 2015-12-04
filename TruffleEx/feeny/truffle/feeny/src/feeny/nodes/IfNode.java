847d9358b2bd06ce8aabb3908f360aa55e2d01b9
Ben Mehne <bmehne@gmail.com>
2015-12-03 23:53:34 -0800

    Missed in commit

---
+++ TruffleEx/feeny/truffle/feeny/src/feeny/nodes/IfNode.java
@@ -0,0 +1,31 @@
+package feeny.nodes;
+
+import com.oracle.truffle.api.RootCallTarget;
+import com.oracle.truffle.api.Truffle;
+import com.oracle.truffle.api.frame.FrameDescriptor;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.RootNode;
+
+import feeny.Feeny;
+
+public class IfNode extends RootNode {
+    @Child RootNode pred_n;
+    @Child RootNode then_n;
+    @Child RootNode else_n;
+
+    public IfNode(RootNode pred_, RootNode then_, RootNode else_, FrameDescriptor frameDescriptor) {
+        super(Feeny.class, null, frameDescriptor);
+        this.pred_n = pred_;
+        this.then_n = then_;
+        this.else_n = else_;
+    }
+
+    @Override
+    public Object execute(VirtualFrame frame) {
+        if (pred_n.execute(frame) == null) {
+            return else_n.execute(frame);
+        } else {
+            return then_n.execute(frame);
+        }
+    }
+}
---
+++ TruffleEx/feeny/truffle/feeny/src/feeny/nodes/WhileNode.java
@@ -0,0 +1,29 @@
+package feeny.nodes;
+
+import com.oracle.truffle.api.RootCallTarget;
+import com.oracle.truffle.api.Truffle;
+import com.oracle.truffle.api.frame.FrameDescriptor;
+import com.oracle.truffle.api.frame.VirtualFrame;
+import com.oracle.truffle.api.nodes.RootNode;
+
+import feeny.Feeny;
+
+public class WhileNode extends RootNode {
+    @Child RootNode pred_n;
+    @Child RootNode body_n;
+
+    public WhileNode(RootNode pred_, RootNode then_, RootNode else_, FrameDescriptor frameDescriptor) {
+        super(Feeny.class, null, frameDescriptor);
+        this.pred_n = pred_;
+        this.body_n = then_;
+    }
+
+    @Override
+    public Object execute(VirtualFrame frame) {
+        Object result = null;
+        while (pred_n.execute(frame) != null) {
+            result = body_n.execute(frame);
+        }
+        return result;
+    }
+}
